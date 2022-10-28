7663ec862ad08915bf39cfb59fbce98af224b57d
Pavel Glavatskikh <byba1100@mail.ru>
2022-10-28 16:36:20 +0400

    CRUD. Методы UPDATE, DELETE

--- src/main/java/ru/glavatskikh/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/glavatskikh/springcourse/controllers/PeopleController.java
@@ -11,7 +11,6 @@ import ru.glavatskikh.springcourse.models.Person;
 @RequestMapping("/people")
 public class PeopleController {
 
-
     private PersonDAO personDAO;
 
     @Autowired
@@ -20,13 +19,13 @@ public class PeopleController {
     }
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
+    public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
@@ -41,4 +40,23 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
+
 }
